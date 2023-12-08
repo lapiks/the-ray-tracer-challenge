916b056526b6541498b1b6a33edc32599cc4cce1
lapiks <dhenin.julien@gmail.com>
2023-12-08 13:02:05 +0100

    intersections sort

--- src/intersection.rs
+++ src/intersection.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use crate::shapes::shape::ShapeRef;
 
 
@@ -16,6 +18,28 @@ impl<'a> Intersection<'a> {
     }
 }
 
+impl<'a> PartialOrd for Intersection<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<'a> Ord for Intersection<'a> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.t < other.t {
+            return Ordering::Less;
+        }
+        else if self.t > other.t {
+            return Ordering::Greater;
+        }
+        else {
+            return Ordering::Equal;
+        }
+    }
+}
+
+impl<'a> Eq for Intersection<'a> {}
+
 pub struct Intersections<'a> {
     intersections: Vec<Intersection<'a>>,
 }
@@ -27,8 +51,9 @@ impl<'a> Intersections<'a> {
         }
     }
 
-    pub fn sort(&mut self) {
-        //self.intersections.sort();
+    pub fn sort(mut self) -> Self {
+        self.intersections.sort();
+        self
     }
 
     pub fn hit(&self) -> Option<&Intersection> {
@@ -68,5 +93,47 @@ mod tests {
         assert_eq!(xs.count(), 2);
         assert_eq!(xs.get(0), Some(&i1));
         assert_eq!(xs.get(1), Some(&i2));
+    }
+
+    #[test]
+    fn hit_when_all_intersections_have_positive_t() {
+        let s = Sphere::default();
+        let i1 = Intersection::new(1.0, ShapeRef::Sphere(&s));
+        let i2 = Intersection::new(2.0, ShapeRef::Sphere(&s));
+        let xs = Intersections::new(vec![i2, i1]).sort();
+        let i = xs.hit();
+        assert_eq!(i, Some(&i1));
+    }
+
+    #[test]
+    fn hit_when_some_intersections_have_negative_t() {
+        let s = Sphere::default();
+        let i1 = Intersection::new(-1.0, ShapeRef::Sphere(&s));
+        let i2 = Intersection::new(1.0, ShapeRef::Sphere(&s));
+        let xs = Intersections::new(vec![i2, i1]).sort();
+        let i = xs.hit();
+        assert_eq!(i, Some(&i2));
+    }
+
+    #[test]
+    fn hit_when_all_intersections_have_negative_t() {
+        let s = Sphere::default();
+        let i1 = Intersection::new(-2.0, ShapeRef::Sphere(&s));
+        let i2 = Intersection::new(-1.0, ShapeRef::Sphere(&s));
+        let xs = Intersections::new(vec![i2, i1]).sort();
+        let i = xs.hit();
+        assert_eq!(i, None);
+    }
+
+    #[test]
+    fn hit_is_always_the_lowest_nonnegative_intersection() {
+        let s = Sphere::default();
+        let i1 = Intersection::new(5.0, ShapeRef::Sphere(&s));
+        let i2 = Intersection::new(7.0, ShapeRef::Sphere(&s));
+        let i3 = Intersection::new(-3.0, ShapeRef::Sphere(&s));
+        let i4 = Intersection::new(2.0, ShapeRef::Sphere(&s));
+        let xs = Intersections::new(vec![i1, i2, i3, i4]).sort();
+        let i = xs.hit();
+        assert_eq!(i, Some(&i4));
     }
 }
