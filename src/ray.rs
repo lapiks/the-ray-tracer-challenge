7b0e598dbbff763847afaf554dbb3b503964343f
lapiks <dhenin.julien@gmail.com>
2023-12-08 13:37:40 +0100

    Ray transformation

--- src/ray.rs
+++ src/ray.rs
@@ -1,4 +1,4 @@
-use glam::Vec3;
+use glam::{Vec3, Mat4};
 
 pub struct Ray {
     pub origin: Vec3,
@@ -17,13 +17,18 @@ impl Ray {
         self.origin + self.direction * t
     }
 
-    pub fn intersect(&self) {
-
+    pub fn transform(&self, mat: &Mat4) -> Ray {
+        Ray {
+            origin:  mat.transform_point3(self.origin),
+            direction: mat.transform_vector3(self.direction)
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use glam::Mat4;
+
     use super::*;
 
     // Creating and quering a ray
@@ -39,10 +44,35 @@ mod tests {
     // Computing a point from a distance
     #[test]
     fn ray_at() {
-        let r = Ray::new(&Vec3::new(2.0, 3.0, 4.0), &Vec3::new(1.0, 0.0, 0.0));
+        let r = Ray::new(
+            &Vec3::new(2.0, 3.0, 4.0), 
+            &Vec3::new(1.0, 0.0, 0.0)
+        );
         assert_eq!(r.at(0.0), Vec3::new(2.0, 3.0, 4.0));
         assert_eq!(r.at(1.0), Vec3::new(3.0, 3.0, 4.0));
         assert_eq!(r.at(-1.0), Vec3::new(1.0, 3.0, 4.0));
         assert_eq!(r.at(2.5), Vec3::new(4.5, 3.0, 4.0));
+    }
+
+    #[test]
+    fn translating_a_ray() {
+        let r = Ray::new(
+            &Vec3::new(1.0, 2.0, 3.0), 
+            &Vec3::new(0.0, 1.0, 0.0)
+        );
+        let r2 = r.transform(&Mat4::from_translation(Vec3::new(3.0, 4.0, 5.0)));
+        assert_eq!(r2.origin, Vec3::new(4.0, 6.0, 8.0));
+        assert_eq!(r2.direction, Vec3::new(0.0, 1.0, 0.0));
+    }
+
+    #[test]
+    fn scaling_a_ray() {
+        let r = Ray::new(
+            &Vec3::new(1.0, 2.0, 3.0), 
+            &Vec3::new(0.0, 1.0, 0.0)
+        );
+        let r2 = r.transform(&Mat4::from_scale(Vec3::new(2.0, 3.0, 4.0)));
+        assert_eq!(r2.origin, Vec3::new(2.0, 6.0, 12.0));
+        assert_eq!(r2.direction, Vec3::new(0.0, 3.0, 0.0));
     }
 }
