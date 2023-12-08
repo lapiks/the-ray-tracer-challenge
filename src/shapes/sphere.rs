5e33d1b9eabdd138fbe5e8451fbbd91df97aca6f
lapiks <dhenin.julien@gmail.com>
2023-12-08 10:50:00 +0100

    take into account sphere radius in intersection function

--- src/shapes/sphere.rs
+++ src/shapes/sphere.rs
@@ -19,7 +19,7 @@ impl Sphere {
         let sphere_to_ray = ray.origin - self.position;
         let a = ray.direction.dot(ray.direction);
         let b = 2.0 * ray.direction.dot(sphere_to_ray);
-        let c = sphere_to_ray.dot(sphere_to_ray) - 1.0;
+        let c = sphere_to_ray.dot(sphere_to_ray) - self.radius * self.radius;
         let discriminant = b*b - 4.0 * a * c;
         if discriminant < 0.0 {
             return Vec::default();
