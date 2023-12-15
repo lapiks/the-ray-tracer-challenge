db62fff76ad4295dbc39e9d640a35a708c67327d
lapiks <dhenin.julien@gmail.com>
2023-12-15 15:18:15 +0100

    Add example04 showing reflective materials + update example02

--- examples/example04.rs
+++ examples/example04.rs
@@ -1,15 +1,15 @@
 use std::f64::consts::PI;
 
 use glam::{dvec3, DMat4};
-use ray_tracer::{Camera, World, Object, shapes::{Sphere, Shape, Plane}, Color, Material, PointLight, transformations, Pattern, pattern::{PlainPattern, PatternObject, StrippedPattern, GradientPattern, CheckerPattern}};
+use ray_tracer::{Camera, World, Object, shapes::{Sphere, Shape, Plane}, Color, Material, PointLight, transformations, Pattern, pattern::{PlainPattern, PatternObject, StrippedPattern, CheckerPattern}};
 
 fn main() {
     let l1 = PointLight::new(
-        dvec3(-10.0, 10.0, -10.0), 
+        dvec3(-5.0, 5.0, -5.0), 
         Color::white() * 0.6
     );
     let l2 = PointLight::new(
-        dvec3(10.0, 10.0, -10.0), 
+        dvec3(5.0, 5.0, -5.0), 
         Color::white() * 0.4
     );
 
@@ -38,6 +38,10 @@ fn main() {
                 )
         );
 
+    let roof = Object::new(Shape::Plane(Plane::default()))
+        .with_material(wall_material.clone())
+        .with_translation(0.0, 10.0, 0.0);
+
     let left_wall = Object::new(Shape::Plane(Plane::default()))
         .with_material(wall_material.clone())
         .with_rotation_x(PI/2.0)
@@ -50,13 +54,25 @@ fn main() {
         .with_rotation_y(PI/4.0)
         .with_translation(0.0, 0.0, 5.0);
 
+    let back_left_wall = Object::new(Shape::Plane(Plane::default()))
+        .with_material(wall_material.clone())
+        .with_rotation_x(PI/2.0)
+        .with_rotation_y(PI/4.0)
+        .with_translation(0.0, 0.0, -15.0);
+
+    let back_right_wall = Object::new(Shape::Plane(Plane::default()))
+        .with_material(wall_material.clone())
+        .with_rotation_x(PI/2.0)
+        .with_rotation_y(-PI/4.0)
+        .with_translation(0.0, 0.0, -15.0);
+
     let middle = Object::new(Shape::Sphere(Sphere::default()))
         .with_material(
             Material::default()
                 .with_pattern(
                     PatternObject::new(
-                        Pattern::Gradient(
-                            GradientPattern::new(Color::new(0.1, 1.0, 0.5), Color::new(0.5, 0.5, 1.0)))
+                        Pattern::Plain(
+                            PlainPattern::new(Color::black()))
                     )
                     .with_transform(
                         &(
@@ -66,8 +82,10 @@ fn main() {
                         )
                     )
                 )
-                .with_diffuse(1.0)
-                .with_specular(0.3)
+                .with_diffuse(0.0)
+                .with_specular(0.0)
+                .with_ambient(0.0)
+                .with_reflective(1.0)
         )
         .with_translation(-0.5, 1.0, 0.5);
 
@@ -87,8 +105,10 @@ fn main() {
                         )
                     )
                 )
-                .with_diffuse(1.0)
-                .with_specular(0.3)
+                .with_diffuse(0.9)
+                .with_specular(1.0)
+                .with_shininess(400.0)
+                .with_reflective(0.1)
         )
         .with_scale(0.5, 0.5, 0.5)
         .with_translation(1.5, 0.5, -0.5);
@@ -98,17 +118,18 @@ fn main() {
             Material::default()
                 .with_pattern(
                     PatternObject::new(
-                        Pattern::Plain(PlainPattern::new(Color::new(1.0, 0.8, 0.1)))
+                        Pattern::Plain(PlainPattern::new(Color::new(1.0, 1.0, 0.0)))
                     )
                 )
-                .with_diffuse(1.0)
-                .with_specular(0.3)
+                .with_diffuse(0.2)
+                .with_specular(0.0)
+                .with_reflective(0.8)
         )
         .with_scale(0.33, 0.33, 0.33)
         .with_translation(-1.5, 0.33, -0.75);
 
     let world = World::new()
-        .with_objects(vec![floor, left_wall, right_wall, middle, right, left])
+        .with_objects(vec![floor, roof, left_wall, right_wall, back_left_wall, back_right_wall, middle, right, left])
         .with_lights(vec![l1, l2]);
 
     let camera = Camera::new(1920, 1080, PI / 3.0)
@@ -121,5 +142,5 @@ fn main() {
         );
 
     let canvas = camera.render(&world, 5);
-    canvas.export("examples/example03.png").unwrap();
+    canvas.export("examples/example04.png").unwrap();
 }
