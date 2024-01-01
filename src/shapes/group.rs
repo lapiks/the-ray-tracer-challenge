02538ba3433176a1daef4977db204a343525c700
lapiks <dhenin.julien@gmail.com>
2024-01-01 18:35:30 +0100

    Groups

--- src/shapes/group.rs
+++ src/shapes/group.rs
@@ -31,12 +31,16 @@ impl Group {
     pub fn new() -> Self {
         Self::default()
     }
+
+    pub fn add_object(&mut self, object: Object) {
+        self.objects.push(object);
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use glam::DMat4;
-    use crate::shapes::Shape;
+    use crate::shapes::{Shape, Sphere};
 
     use super::*;
 
@@ -46,5 +50,14 @@ mod tests {
         let o = Object::new(Shape::Group(g.clone()));
         assert_eq!(*o.transform(), DMat4::IDENTITY);
         assert_eq!(g.objects.len(), 0);
+    }
+
+    #[test]
+    fn adding_a_child_to_a_group() {
+        let mut g = Group::default();
+        let s = Object::new(Shape::Sphere(Sphere::default()));
+        g.add_object(s.clone());
+        assert_eq!(g.objects.len(), 1);
+        assert_eq!(g.objects[0], s);
     }
 }
